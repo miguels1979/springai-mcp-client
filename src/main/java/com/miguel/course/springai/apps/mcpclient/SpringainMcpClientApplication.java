11152d438b248969f12f3034e0afa3741cd1cd1a
Miguel Angel Sosa <gravitones@hotmail.com>
2026-09-09 11:01:13 -0600

    first commit

---
+++ src/main/java/com/miguel/course/springai/apps/mcpclient/SpringainMcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.course.springai.apps.mcpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringainMcpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringainMcpClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miguel/course/springai/apps/mcpclient/SpringainMcpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.course.springai.apps.mcpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringainMcpClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
